6a7418904930bbe597d5b0143fd4a01a9b966732
Grimoldi Eugenio <e.grimoldi@blueit.it>
2023-12-01 16:35:41 +0100

    Added first day.

---
+++ src/01.py
@@ -0,0 +1,96 @@
+import os
+
+from utils import load_input_data
+
+DAY = os.path.basename(__file__).split(".")[0]
+
+
+def main():
+    # first_question(False)
+    second_question(False)
+
+
+def first_question(test: bool = False) -> None:
+    """Answers to the first question."""
+    data = load_input_data(DAY, test)
+    digits = find_double_digit_coordinate(data)
+    print(f"{[x for x in digits]}\n{sum(digits)}")
+
+
+def second_question(test: bool = False) -> None:
+    """Answers to the second question."""
+    data = load_input_data(DAY, test)
+    digits = find_double_digit_coordinate_even_from_string(data)
+    print(f"{[x for x in digits]}\n{sum(digits)}")
+
+
+def find_double_digit_coordinate(data: list[str]) -> list[int]:
+    """From the raw data extract the double digits coordinates."""
+    digits: list[int] = list()
+    for line in data:
+        numbers = list()
+        for char in line:
+            try:
+                digit = int(char)
+                numbers.append(digit)
+            except ValueError as _:
+                continue
+
+        digits.append(get_digits_from_list(numbers))
+
+    return digits
+
+
+def find_double_digit_coordinate_even_from_string(data: list[str]) -> list[int]:
+    """From the raw data extract the double digits coordinates. First ten numbers are also valid as strings."""
+
+    def convert_name_to_number(name: str) -> int:
+        """Convert the name to the number."""
+        return MAPPING[name]
+
+    def match_key_name(substring: str) -> bool:
+        """Checks if a substring matches a key name."""
+        matched = [x for x in MAPPING.keys() if x.startswith(substring)]
+        return len(matched) > 0
+
+    MAPPING = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+    digits: list[int] = list()
+
+    for line in data:
+        numbers = list()
+        for index, char in enumerate(line):
+            try:
+                digit = int(char)
+                numbers.append(digit)
+            except ValueError as _:
+                i = 1
+                substring = line[index : index + i]
+                while match_key_name(substring) and i <= len(line):
+                    i += 1
+                    substring = line[index : index + i]
+                word_number = line[index : index + i - 1]
+                if word_number in MAPPING:
+                    numbers.append(convert_name_to_number(word_number))
+
+        digits.append(get_digits_from_list(numbers))
+
+    return digits
+
+
+def get_digits_from_list(numbers: list[int]) -> int:
+    """Gets a double digits number from a list of int."""
+    return 10 * numbers[0] + numbers[-1]
+
+
+if __name__ == "__main__":
+    main()
--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,14 @@ import os
 from enum import Enum
 from pathlib import Path
 
-BASE_PATH = Path(__file__).parent
+BASE_PATH = Path(__file__).parent.parent
 LOGGER_NAME = "AOC"
+
 
 class LogLevel(Enum):
     DEBUG = 0
     INFO = 1
+
 
 def setup_logger(level: LogLevel) -> logging.Logger:
     """Creates a logger instance."""
@@ -19,9 +21,10 @@ def setup_logger(level: LogLevel) -> logging.Logger:
 
     return logger
 
+
 def _logger_handler() -> logging.StreamHandler:
     """Setup the default stream handler."""
-    log_format = '%(levelname)s:: %(message)s-%(funcName)s-%(name)s-%(lineno)d'
+    log_format = "%(levelname)s:: %(message)s-%(funcName)s-%(name)s-%(lineno)d"
     formatter = logging.Formatter(log_format)
 
     handler = logging.StreamHandler()
@@ -29,17 +32,19 @@ def _logger_handler() -> logging.StreamHandler:
 
     return handler
 
+
 def create_log_level(debug: bool) -> LogLevel:
     """Factory for logging level."""
     if debug:
         return LogLevel.DEBUG
     return LogLevel.INFO
 
-def load_input_data(day: str, debug: bool = False) -> list[str]:
+
+def load_input_data(day: str, test: bool = False) -> list[str]:
     """Load all data from a file."""
     data_file = f"{day}_input.txt"
-    if debug:
-        data_file = f"{day}_example.txt"
+    if test:
+        data_file = f"{day}_test.txt"
     data_path = BASE_PATH / "input_data" / data_file
 
     if not os.path.exists(data_path):
