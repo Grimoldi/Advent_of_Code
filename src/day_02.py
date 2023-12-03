5e65127454035e7ba9bc146a21e0526de1b4b52a
Grimoldi Eugenio <e.grimoldi@blueit.it>
2023-12-03 12:11:11 +0100

    Added day 02.

---
+++ src/day_02.py
@@ -0,0 +1,148 @@
+import re
+from dataclasses import dataclass
+from enum import StrEnum, auto
+
+from utils import load_input_data
+
+
+class Dice(StrEnum):
+    BLUE = auto()
+    RED = auto()
+    GREEN = auto()
+
+
+@dataclass
+class SingleRound:
+    greens: int
+    reds: int
+    blues: int
+
+
+@dataclass
+class Game:
+    game_id: int
+    rounds: list[SingleRound]
+    max_reds: int = 12
+    max_greens: int = 13
+    max_blues: int = 14
+
+    @property
+    def is_valid(self) -> bool:
+        """Checks if the game respects its limits."""
+        is_valid = True
+        for single_round in self.rounds:
+            if (
+                single_round.blues > self.max_blues
+                or single_round.greens > self.max_greens
+                or single_round.reds > self.max_reds
+            ):
+                is_valid = False
+                break
+
+        return is_valid
+
+    @property
+    def min_blues_for_valid_game(self) -> int:
+        """Returns the minimum number of blue dices for the game to be valid."""
+        return max([x.blues for x in self.rounds])
+
+    @property
+    def min_greens_for_valid_game(self) -> int:
+        """Returns the minimum number of green dices for the game to be valid."""
+        return max([x.greens for x in self.rounds])
+
+    @property
+    def min_reds_for_valid_game(self) -> int:
+        """Returns the minimum number of red dices for the game to be valid."""
+        return max([x.reds for x in self.rounds])
+
+    @property
+    def power(self) -> int:
+        """Returns the power of the game."""
+        return (
+            self.min_reds_for_valid_game
+            * self.min_blues_for_valid_game
+            * self.min_greens_for_valid_game
+        )
+
+
+def parse_game(game: str) -> Game:
+    """Parse a game from raw data"""
+    game_id = get_game_id(game)
+    rounds_line = re.sub(r"Game \d+\: ", "", game)
+    game_rounds = rounds_line.split(";")
+    round_dices: list[SingleRound] = list()
+    for game_round in game_rounds:
+        single_round = get_dices_from_single_round(game_round)
+        round_dices.append(single_round)
+
+    return Game(game_id, round_dices)
+
+
+def get_game_id(line: str) -> int:
+    """Get the raw game id."""
+    game_compiler = re.compile(r"Game (?P<game_id>\d+)\:")
+    regex_group = game_compiler.search(line)
+    if regex_group is None:
+        raise ValueError(f"Unable to find Game id in line {line}")
+
+    return int(regex_group.group("game_id"))
+
+
+def get_dices_from_single_round(game_round: str) -> SingleRound:
+    """From a string round get the SingleRound object."""
+    blues_compiler = re.compile(r"(.*\, )?(?P<blues>\d+ blue).*$")
+    blues_group = blues_compiler.search(game_round)
+    blues = get_dice_from_regex(blues_group, Dice.BLUE)
+
+    greens_compiler = re.compile(r"(.*\, )?(?P<greens>\d+ green).*$")
+    greens_group = greens_compiler.search(game_round)
+    greens = get_dice_from_regex(greens_group, Dice.GREEN)
+
+    reds_compiler = re.compile(r"(.*\, )?(?P<reds>\d+ red).*$")
+    reds_group = reds_compiler.search(game_round)
+    reds = get_dice_from_regex(reds_group, Dice.RED)
+
+    return SingleRound(greens=greens, reds=reds, blues=blues)
+
+
+def get_dice_from_regex(matched: re.Match | None, color: Dice) -> int:
+    """Gets the number of dice from the regex."""
+    if matched is None:
+        return 0
+
+    matched_group = matched.group(f"{color.value}s")
+    if matched_group is None:
+        return 0
+
+    return int(matched_group.replace(color.value, "").strip())
+
+
+def main():
+    first_question()
+    second_question()
+
+
+def first_question() -> None:
+    """Answers to the first question."""
+    data = load_input_data("02_input")
+    games: list[Game] = list()
+    for game in data:
+        games.append(parse_game(game))
+
+    valid_games = [x for x in games if x.is_valid]
+    print(f"Valid games ID sum is: {sum(x.game_id for x in valid_games)}")
+
+
+def second_question() -> None:
+    """Answers to the second question."""
+    data = load_input_data("02_input")
+    games: list[Game] = list()
+    for game in data:
+        games.append(parse_game(game))
+
+    print(f"Total games power is: {sum(x.power for x in games)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_02.py
@@ -0,0 +1,117 @@
+import re
+
+import day_02
+from utils import load_input_data
+
+
+def test_game_id() -> None:
+    """Test that given a list of single digits integers, the double digit number is what's expected."""
+    data = load_input_data("02_test")
+    game_id = day_02.get_game_id(data[3])
+    assert game_id == 4
+
+
+def test_regex_for_round() -> None:
+    """Test that the group regex is working as expected."""
+    test_round = "3 green, 4 red"
+
+    blues_compiler = re.compile(r".*(?P<blues>\d+ blue).*$")
+    blues_group = blues_compiler.search(test_round)
+    blues = day_02.get_dice_from_regex(blues_group, day_02.Dice.BLUE)
+    assert blues == 0
+
+    greens_compiler = re.compile(r".*(?P<greens>\d+ green).*$")
+    greens_group = greens_compiler.search(test_round)
+    greens = day_02.get_dice_from_regex(greens_group, day_02.Dice.GREEN)
+    assert greens == 3
+
+    reds_compiler = re.compile(r".*(?P<reds>\d+ red).*$")
+    reds_group = reds_compiler.search(test_round)
+    reds = day_02.get_dice_from_regex(reds_group, day_02.Dice.RED)
+    assert reds == 4
+
+
+def test_singleround_from_raw() -> None:
+    """Test the parsing of a single round."""
+    test_rounds = ["3 blue, 4 red", "1 red, 2 green, 6 blue", "2 green"]
+    expected_single_rounds = [
+        day_02.SingleRound(greens=0, reds=4, blues=3),
+        day_02.SingleRound(greens=2, reds=1, blues=6),
+        day_02.SingleRound(greens=2, reds=0, blues=0),
+    ]
+    for index, test_round in enumerate(test_rounds):
+        actual_round = day_02.get_dices_from_single_round(test_round)
+        assert actual_round == expected_single_rounds[index]
+
+
+def test_game() -> None:
+    """Test the parsing of a single game."""
+    test_game = "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    rounds = [
+        day_02.SingleRound(reds=6, blues=1, greens=3),
+        day_02.SingleRound(reds=1, blues=2, greens=2),
+    ]
+    expected_game = day_02.Game(game_id=5, rounds=rounds)
+    game = day_02.parse_game(test_game)
+
+    assert game == expected_game
+
+
+def test_valid_game() -> None:
+    """Test if a valid game."""
+    test_game = "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    game = day_02.parse_game(test_game)
+
+    assert game.is_valid == True
+
+
+def test_invalid_game() -> None:
+    """Test an invalid game."""
+    test_game = (
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+    )
+    game = day_02.parse_game(test_game)
+
+    assert game.is_valid == False
+
+
+def test_first_question() -> None:
+    """Test first question."""
+
+    data = load_input_data("02_test")
+    games: list[day_02.Game] = list()
+    for game in data:
+        games.append(day_02.parse_game(game))
+
+    valid_games = [x for x in games if x.is_valid]
+    game_id_sum = sum(x.game_id for x in valid_games)
+    assert game_id_sum == 8
+
+
+def test_minimum_red_dices_for_valid_game() -> None:
+    """Test the minimum number of red dice for a game to be valid."""
+    test_game = (
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+    )
+    game = day_02.parse_game(test_game)
+
+    assert game.min_reds_for_valid_game == 20
+
+
+def test_power_of_a_game() -> None:
+    """Tests the power of a game."""
+    test_game = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+    game = day_02.parse_game(test_game)
+
+    assert game.power == 48
+
+
+def test_second_question() -> None:
+    """Test second question."""
+    data = load_input_data("02_test")
+    games: list[day_02.Game] = list()
+    for game in data:
+        games.append(day_02.parse_game(game))
+
+    game_power_sum = sum(x.power for x in games)
+    assert game_power_sum == 2286
